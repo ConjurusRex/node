6b2deda7af7e5dde870549161f473e0789ff0f40
ConjurusRex <jnebeker91@protonmail.com>
2022-03-19 22:51:52 -0600

    added items array and submit button

--- controllers/store.controllers.js
+++ controllers/store.controllers.js
@@ -1,3 +1,11 @@
+const items = [
+  {
+    title: 'Wand',
+    price: '1',
+    desc: 'A really great wand for witches and wizzards'
+  }
+]
+
 exports.addItem = (req, res, next) => {
   console.log('store.controller - addItem()');
 };
