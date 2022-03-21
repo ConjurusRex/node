f5f747a9f9737760f6707562bd0b07c6dc2053ad
ConjurusRex <jnebeker91@protonmail.com>
2022-03-20 18:22:09 -0600

    items array and addItem logic

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const { dirname } = require('path');
 const path = require('path');
 const app = express();
@@ -6,6 +7,7 @@ const port = 3000;
 
 const storeRoutes = require('./routes/store.routes');
 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'views')));
 app.use(storeRoutes);
 
--- controllers/store.controllers.js
+++ controllers/store.controllers.js
@@ -2,14 +2,25 @@ const items = [
   {
     title: 'Wand',
     price: '1',
-    desc: 'A really great wand for witches and wizzards'
-  }
-]
+    desc: 'A really great wand for witches and wizzards',
+  },
+];
 
 exports.addItem = (req, res, next) => {
   console.log('store.controller - addItem()');
+  try {
+    const item = {
+      title: req.body.title,
+      desc: req.body.desc,
+    };
+    items.push(item);
+    console.log(`item was successfully added: ${item.title} - ${item.desc}`);
+    res.redirect('/');
+  } catch (e) {
+    throw new Error(`item was not added: ${e}`);
+  }
 };
 
 exports.getItems = (req, res, next) => {
   res.render('index.html');
-}
+};
